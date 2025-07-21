0e41e2bf1312c90c72c27fde8179e2ab1b09b04c
Mai Reaksa <mai.reaksa@sabay.com>
2025-07-21 10:56:14 +0700

    create file test function go

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 import "fmt"
+import "log"
 func main() {
 	fmt.Println("Hello, Gateway One App!")
+}
+func LogError(err error) {
+	if err != nil {
+		log.Printf("Error: %v", err)
+	}
+}
+func LogInfo(message string) {
+	log.Printf("Info: %s", message)
+}
+func LogDebug(message string) {
+	log.Printf("Debug: %s", message)
+}
+func LogWarning(message string) {
+	log.Printf("Warning: %s", message)
+}
+func LogFatal(err error) {
+	if err != nil {
+		log.Fatalf("Fatal error: %v", err)
+	}
 }
