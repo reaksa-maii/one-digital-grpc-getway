6a3012b2038f81a4c74efff7dbcd9183d9e90ea5
Mai Reaksa <mai.reaksa@sabay.com>
2025-07-23 16:59:12 +0700

    add remote build package

---
+++ remote/remote.go
@@ -0,0 +1,11 @@
+package remote;
+
+import ("fmt")
+
+func remoteBookStub(books string) {
+	fmt.Print("Product", books)
+}
+
+func remotePodcastStub(podcasts string) {
+	fmt.Print("Product", podcasts)
+}
