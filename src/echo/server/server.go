25611016b5a7a93b5a4edee5a748458864cd8b45
Mai Reaksa <mai.reaksa@sabay.com>
2025-07-30 16:34:55 +0700

    implement code status

--- src/echo/server/server.go
+++ src/echo/server/server.go
@@ -9,22 +9,29 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	pb "github.com/reaksa-maii/one_digital_grpc_getway/proto/echo/v2"
 )
 
-var ports = flag.Int("port", 50052, "port server run time")
+var (
+	ports = flag.Int("port", 50052, "port server run time")
+	errMassaeg = status.Errorf(codes.InvalidArgument, "missing matada")
+	errInvalideToken = status.Errorf(codes.Unauthenticated, "invalide token")
+)
 
 type EchoService struct {
 	pb.UnimplementedEchoServer
 }
 
 func (s *EchoService) EchoMessage(ctx context.Context, req *pb.EchoRequest) (*pb.EchoReply, error) {
-	return &pb.EchoReply{Message: "Hello gRPC"}, nil
+	return &pb.EchoReply{Message: fmt.Sprintf("Message, %s", req.Message)}, nil
 }
 
 func main() {
-
+	fmt.Print(errMassaeg)
+	fmt.Print(errInvalideToken)
 	flag.Parse()
 	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *ports))
 	if err != nil {
