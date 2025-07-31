c8424ba4aa86ffd10e73c9b9be769c3a224e7749
Mai Reaksa <mai.reaksa@sabay.com>
2025-07-31 15:43:20 +0700

    add book code gen v3

---
+++ app/books/server/bookCancellation.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+
+	pb "github.com/reaksa-maii/one_digital_grpc_getway/proto/book/v3"
+)
