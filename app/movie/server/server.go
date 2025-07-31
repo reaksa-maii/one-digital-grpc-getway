86e713e9f6f3dfee56009be4c8f97a5f54443453
Mai Reaksa <mai.reaksa@sabay.com>
2025-07-31 13:27:32 +0700

    implement server runtime call stub

--- app/movie/server/server.go
+++ app/movie/server/server.go
@@ -1,4 +1,4 @@
-package server
+package main
 
 import (
 	"context"
@@ -9,14 +9,30 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 
 	pb "github.com/reaksa-maii/one_digital_grpc_getway/proto/movie/v1"
 )
 
-var (
-	ports = flag.Int("port", 50052, "port server run time")
-	errMassaeg = status.Errorf(codes.InvalidArgument, "missing matada")
-	errInvalideToken = status.Errorf(codes.Unauthenticated, "invalide token")
-)
+var ports = flag.Int("port", 50051, "port server run time")
+
+type server struct {
+	pb.UnimplementedMovieServer
+}
+
+func (s *server) UnaryMovie(_ context.Context, in *pb.MovieRequest) (*pb.MovieResponse, error) {
+	fmt.Printf("Unary Movie Massage: %q\n", in.Title)
+	return &pb.MovieResponse{Movie: in.Movie}, nil
+}
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *ports))
+	if err != nil {
+		fmt.Printf("Server not found! %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterMovieServer(s, &server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
