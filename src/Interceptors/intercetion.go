de2d35e9a4de54e733623893d5c11bec1cd718e7
Mai Reaksa <mai.reaksa@sabay.com>
2025-07-31 10:31:06 +0700

    implement interceptor code gen vai streaming

--- src/Interceptors/intercetion.go
+++ src/Interceptors/intercetion.go
@@ -2,24 +2,35 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
+	"log"
+	"net"
 	"strings"
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc/status"
-	
+
 	pb "github.com/reaksa-maii/one_digital_grpc_getway/proto/echo/v3"
 )
 
 var (
+	ports            = flag.Int("port", 50051, "port: server")
 	errMassaeg       = status.Errorf(codes.InvalidArgument, "missing invalide")
 	errInvalideToken = status.Error(codes.Unauthenticated, "unautheticate token")
 )
+
 type server struct {
-	
+	pb.UnimplementedEchoServer
+}
+
+func (s *server) UnaryEcho(_ context.Context, in *pb.EchoRequest) (*pb.EchoResponse, error) {
+	fmt.Printf("Unary echo message: %q\n", in.Movie)
+	return &pb.EchoResponse{Movie: in.Movie}, nil
 }
 func logging(format string, a ...any) {
 	fmt.Printf("Log Error: \t"+format+"\n", a...)
@@ -63,4 +74,31 @@ func (w *wrappedStream) SendMsg(m any) error {
 
 func newWrappedStream(s grpc.ServerStream) grpc.ServerStream {
 	return &wrappedStream{s}
+}
+func streamInterceptor(srv any, ss grpc.ServerStream, _ *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+	md, ok := metadata.FromIncomingContext(ss.Context())
+	if !ok {
+		return errMassaeg
+	}
+	if !validate(md["authorization"]) {
+		return errInvalideToken
+	}
+	err := handler(srv, newWrappedStream(ss))
+	if err != nil {
+		logging("gRPC streaming error with: %v", err)
+	}
+	return err
+}
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *ports))
+	if err != nil {
+		log.Fatalf("faild to listent port: %v", err)
+	}
+	s := grpc.NewServer(grpc.StreamInterceptor(streamInterceptor), grpc.UnaryInterceptor(unaryIntercepter))
+	reflection.Register(s)
+	pb.RegisterEchoServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("fial server stating from! %v", err)
+	}
 }
