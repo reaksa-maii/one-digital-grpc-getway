2a077b5a49f7ec283d3dfe3f7d769451df8dfa93
Mai Reaksa <mai.reaksa@sabay.com>
2025-08-06 11:40:20 +0700

    update port status

--- gateway/restGetway.go
+++ gateway/restGetway.go
@@ -3,8 +3,8 @@ package gateway
 import (
 	"context"
 	"fmt"
-	"net/http"
 	"log"
+	"net/http"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	pb "github.com/reaksa-maii/one_digital_grpc_getway/proto/podcast/v1"
@@ -14,12 +14,15 @@ import (
 
 func GetMethod() error {
 	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
 	mux := runtime.NewServeMux()
 
-	conn, err := grpc.NewClient("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.NewClient("localhost:9090", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		return err
 	}
+
 	defer conn.Close()
 
 	if err := pb.RegisterPodcatServiceHandler(ctx, mux, conn); err != nil {
@@ -27,7 +30,7 @@ func GetMethod() error {
 	}
 
 	fmt.Println("Starting RestFull server on localhost:8081...")
-	if err := http.ListenAndServe("localhost:8081", mux); err != nil {
+	if err := http.ListenAndServe(":9090", mux); err != nil {
 		return err
 	}
 	return nil
@@ -36,20 +39,23 @@ func PostMethod() error {
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
-	
 	mux := runtime.NewServeMux()
 
-	opts := []grpc.DialOption{grpc.WithInsecure()} // or use credentials.NewClientTLSFromCert() for secure connection
-
-	err := pb.RegisterPodcatServiceHandlerFromEndpoint(ctx, mux, "localhost:8080", opts)
+	conn, err := grpc.DialContext(ctx, ":8080", grpc.WithInsecure())
 	if err != nil {
 		return err
 	}
 
-	fmt.Println("Starting gRPC-Gateway server on localhost:8081...")
-	return http.ListenAndServe("localhost:8081", mux)
-}
+	if err := pb.RegisterPodcatServiceHandler(ctx, mux, conn); err != nil {
+		return err
+	}
 
+	fmt.Println("Starting Rest-Gateway server on :8081...")
+	if err := http.ListenAndServe(":8081", mux); err != nil {
+		return err
+	}
+	return nil
+}
 func RunRestFull() error {
 	go func() {
 		if err := GetMethod(); err != nil {
@@ -60,5 +66,5 @@ func RunRestFull() error {
 	if err := PostMethod(); err != nil {
 		log.Fatalf("Failed to run gRPC server: %v", err)
 	}
-	return nil;
+	return nil
 }
